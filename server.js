7b1e2f80a83e696aa5ba5d4e2c609239f9ca4660
Ahmad Tariq <engg.ahmd@gmail.com>
2019-06-11 19:10:11 +0500

    rout files with express router

--- server.js
+++ server.js
@@ -6,7 +6,14 @@ const app = express();
 //Connect Database
 connectDB();
 
-app.get("/", (req, res) => res.send("Running Successfully!"));
+app.get('/', (req, res) => res.send('Running Successfully!'));
+
+//Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+
 
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Listening on port ${port}`))
